1060d694e605ba2a2fc64561c074dbf617616bc6
Josh Archer <archerjoshb@gmail.com>
2026-05-19 12:31:03 -0700

    Added support for local wishlists

--- src/routes/localWishlist.js
+++ src/routes/localWishlist.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import logger from '../logger.js'
+import logger from '../../logger.js'
 import { getAll, add, remove } from '../services/localWishlistService.js'
 
 const router = Router()
